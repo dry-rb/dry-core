d5917a73c19465afc7c2c3e11f980d3643dd9acd
Luca Guidi <me@lucaguidi.com>
2021-06-12 16:54:12 +0200

    Ignore inline Style/EmptyElse cop

--- lib/dry/core/class_attributes.rb
+++ lib/dry/core/class_attributes.rb
@@ -67,7 +67,7 @@ module Dry
       #    defines :one, coerce: Dry::Types['coercible.string']
       #  end
       #
-      def defines(*args, type: ::Object, coerce: IDENTITY)
+      def defines(*args, type: ::Object, coerce: IDENTITY) # rubocop:disable Metrics/PerceivedComplexity
         unless coerce.respond_to?(:call)
           raise ::ArgumentError, "Non-callable coerce option: #{coerce.inspect}"
         end
@@ -80,6 +80,8 @@ module Dry
               if Undefined.equal?(value)
                 if instance_variable_defined?(ivar)
                   instance_variable_get(ivar)
+                else # rubocop:disable Style/EmptyElse
+                  nil
                 end
               elsif type === value # rubocop:disable Style/CaseEquality
                 instance_variable_set(ivar, coerce.call(value))
