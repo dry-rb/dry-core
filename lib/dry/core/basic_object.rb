6c8a6fa9141f5297d3b602ad35644fe20b8417fe
Luca Guidi <me@lucaguidi.com>
2022-03-03 09:53:14 +0100

    Use Ruby 2.7+ `Kernel.inspect`

--- lib/dry/core/basic_object.rb
+++ lib/dry/core/basic_object.rb
@@ -5,7 +5,7 @@ module Dry
   module Core
     # BasicObject
     #
-    # @since 0.7.0
+    # @since 0.8.0
     class BasicObject < ::BasicObject
       # Lookups constants at the top-level namespace, if they are missing in the
       # current context.
@@ -16,7 +16,7 @@ module Dry
       #
       # @raise [NameError] if the constant cannot be found
       #
-      # @since 0.7.0
+      # @since 0.8.0
       # @api private
       #
       # @see https://ruby-doc.org/core/Module.html#method-i-const_missing
@@ -26,7 +26,7 @@ module Dry
 
       # Returns the class for debugging purposes.
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-class
       def class
@@ -37,11 +37,13 @@ module Dry
       #
       # @return [String] the inspect string
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-inspect
-      def inspect
-        "#<#{self.class}:#{"0x0000%x" % (__id__ << 1)}#{__inspect}>"
+      inspect_method = ::Kernel.instance_method(:inspect)
+      define_method(:inspect) do
+        original = inspect_method.bind_call(self)
+        "#{original[0...-1]}#{__inspect}>"
       end
 
       # @!macro [attach] instance_of?(class)
@@ -54,7 +56,7 @@ module Dry
       #
       # @raise [TypeError] if the given argument is not of the expected types
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-instance_of-3F
       define_method :instance_of?, ::Object.instance_method(:instance_of?)
@@ -69,7 +71,7 @@ module Dry
       #
       # @raise [TypeError] if the given argument is not of the expected types
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-is_a-3F
       define_method :is_a?, ::Object.instance_method(:is_a?)
@@ -84,7 +86,7 @@ module Dry
       #
       # @raise [TypeError] if the given argument is not of the expected types
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-kind_of-3F
       define_method :kind_of?, ::Object.instance_method(:kind_of?)
@@ -93,7 +95,7 @@ module Dry
       #
       # @return [Fixnum] the object id
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-object_id
       def object_id
@@ -105,7 +107,7 @@ module Dry
       # @param printer [PP] the Pretty Printable printer
       # @return [String] the pretty-printable inspection of the object
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see https://ruby-doc.org/stdlib/libdoc/pp/rdoc/PP.html
       def pretty_print(printer)
@@ -116,7 +118,7 @@ module Dry
       #
       # @return [TrueClass,FalseClass] the result of the check
       #
-      # @since 0.7.0
+      # @since 0.8.0
       #
       # @see http://ruby-doc.org/core/Object.html#method-i-respond_to-3F
       def respond_to?(method_name, include_all = false) # rubocop:disable Style/OptionalBooleanParameter
@@ -127,13 +129,13 @@ module Dry
 
       # Must be overridden by descendants
       #
-      # @since 0.7.0
+      # @since 0.8.0
       # @api private
       def respond_to_missing?(_method_name, _include_all)
         ::Kernel.raise ::NotImplementedError
       end
 
-      # @since 0.7.0
+      # @since 0.8.0
       # @api private
       def __inspect # rubocop:disable Style/EmptyMethod
       end
